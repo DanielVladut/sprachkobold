d437b737436fe7db45d538b89b0bdd0034ee20df
Daniel Vladut <dan.vladut@gmail.com>
2019-09-18 22:30:44 +0200

    added backend with dummy controllers

---
+++ backend/SprachkoboldApi/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SprachkoboldApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        public string GetVersion()
+        {
+            return "v1-0.0.1-alpha";
+        }
+    }
+}
---
+++ backend/SprachkoboldApi/Controllers/NounController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SprachkoboldApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NounController : ControllerBase
+    {
+        [HttpGet("{baseForm}")]
+        public ActionResult<string> Get(string baseForm)
+        {
+            return Ok(baseForm);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(id.ToString());
+        }
+    }
+}
